2bd59b02a0f8f5e73b7b3386cbd269868883d941
VPashutin <pashutin@gmail.com>
2025-01-11 18:38:44 +0500

    Присоединён проект для создания основного сайта

--- yandex_business_ai_landing/model/__init__.py
+++
@@ -1,8 +0,0 @@
-from .user_prompts import UserPrompts
-from .user_file import UserFile
-from .post import Post
-from .long_operation import LongOperation
-from .user import User
-from .review_issue import ReviewIssue
-
-__all__ = ['UserPrompts', 'UserFile', 'Post', 'LongOperation', 'User', 'ReviewIssue']
